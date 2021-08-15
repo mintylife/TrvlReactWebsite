729959115d2fd2633ba76bff9357b1b726f139b8
Dameli <dameli.ryspay@gmail.com>
2021-08-15 23:04:26 +0600

    add footer nav links, finish

--- src/components/pages/Home.page.jsx
+++ src/components/pages/Home.page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../App.css";
 import Cards from "../Cards.component";
+import Footer from "../Footer.component";
 import HeroSection from "../HeroSection.component";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 }
